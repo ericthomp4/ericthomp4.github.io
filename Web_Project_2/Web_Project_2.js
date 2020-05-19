a5850033c094fb3847010d56d9a4556ef0522424
Eric Thompson <Eric@Eric-Thompsons-MacBook.local>
2020-05-19 04:01:19 +0200

    Web_Project_2

---
+++ Web_Project_2/Web_Project_2.js
@@ -0,0 +1,53 @@
+$('#Hide_Train_Map').click(function(){
+  console.log("Hide Button Clicked");
+  $('#Train_Map').hide();
+})
+$('#Show_Train_Map').click(function(){
+  console.log("Show Button Clicked");
+  $('#Train_Map').show();
+})
+
+$('#Show_Hand').click(function(){
+  console.log("Hide Button Clicked");
+  $('#Hand').show();
+})
+$('#Hide_Hand').click(function(){
+  console.log("Show Button Clicked");
+  $('#Hand').hide();
+})
+
+$('#Show_Half').click(function(){
+  console.log("Hide Button Clicked");
+  $('#Half').show();
+})
+$('#Hide_Half').click(function(){
+  console.log("Show Button Clicked");
+  $('#Half').hide();
+})
+
+$('#Show_Mushrooms').click(function(){
+  console.log("Hide Button Clicked");
+  $('#Mushrooms').show();
+})
+$('#Hide_Mushrooms').click(function(){
+  console.log("Show Button Clicked");
+  $('#Mushrooms').hide();
+})
+
+$('#Show_Sunset').click(function(){
+  console.log("Hide Button Clicked");
+  $('#Sunset').show();
+})
+$('#Hide_Sunset').click(function(){
+  console.log("Show Button Clicked");
+  $('#Sunset').hide();
+})
+
+$('#Show_Lady_Face').click(function(){
+  console.log("Hide Button Clicked");
+  $('#Lady_Face').show();
+})
+$('#Hide_Lady_Face').click(function(){
+  console.log("Show Button Clicked");
+  $('#Lady_Face').hide();
+})
